75a949236c0b14740b8fe600ac9ea4e185ddaae3
hilkojj <hilkojj@outlook.com>
2019-05-14 11:43:01 +0200

    repos to huge txt files

---
+++ repo_to_huge_txt.py
@@ -0,0 +1,46 @@
+
+from glob import glob
+import os
+import fnmatch
+import re
+
+extentions = ["bat", "bluej", "c", "cpp", "css", "d", "frag", "glsl", "gradle", "h", "html", "ino", "java", "js", "php", "phpt", "py", "scss", "sh", "sql", "ts", "vert"]
+includes = '|'.join([fnmatch.translate("*." + x) for x in extentions])
+
+projects = ["1-2", "2-1", "2-2", "2-3"]
+os.mkdir("repo_sources")
+
+for project in projects:
+
+    prj_dir = "hanzerepos/" + project
+    os.mkdir("repo_sources/" + project)
+
+    for repo_dir in glob(prj_dir + "/*/"):
+        print("\n\n-------\n", repo_dir)
+
+        src_files = []
+        src_txt = ""
+
+        for root, dirs, files in os.walk(repo_dir):
+            src_files.extend(
+                [
+                    (root if root.endswith("/") else root + "/") 
+                        + f 
+                    
+                    for f in files 
+                    if re.match(includes, f)
+                ]
+            )
+
+        for f in src_files:
+            opened = open(f, "rb")
+            src = opened.read().decode(errors="replace")
+            src_txt += src
+
+        dst = open(repo_dir.replace("hanzerepos", "repo_sources")[:-1] + ".txt", "w")
+        dst.write(src_txt)
+        dst.close()
+
+
+
+
