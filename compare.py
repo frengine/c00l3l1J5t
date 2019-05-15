58c606d8f015ef3aafa8ea7568babdca3671b355
timostrating <timostrating@home.nl>
2019-05-16 00:25:56 +0200

    slow version of quick lines of code checker

--- compare.py
+++ compare.py
@@ -1,4 +1,4 @@
-def lines(a, b):
+def lines(b, a):
     a_split = a.split("\n")
     b_split = b.split("\n")
     count = 0
@@ -41,8 +41,8 @@ def edit_distance(s1, s2): # optimized levenshtein distance
 
 
 
-str1 = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-str2 = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+# str1 = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+# str2 = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
 
-print(lines(str1, str2), "REGELS ZIJN HET ZELFDE")
-print(edit_distance(str1, str2), "LEVENSTEIN EDIT DISTANCE")
+# print(lines(str1, str2), "REGELS ZIJN HET ZELFDE")
+# print(edit_distance(str1, str2), "LEVENSTEIN EDIT DISTANCE")
--- gspeadsheet.py
+++ gspeadsheet.py
@@ -5,10 +5,13 @@ from glob import glob
 import os
 import subprocess
 
+from compare import edit_distance, lines
 
-def get_to_know_project(project, command):
-    dir_name = url.split(".com/")[1].replace("/", "_")
-    return safe_check(subprocess.check_output, "cd hanzerepos/"+project+"/"+dir_name+" && " + command, shell=True, universal_newlines=True, encoding='utf-8', errors='ignore')
+TO_MUCH_CODE_THRESHHOLD = 7500
+
+
+def exc_command(command):
+    return safe_check(subprocess.check_output, command, shell=True, universal_newlines=True, encoding='utf-8', errors='ignore')
 
 def safe_check(function, *args, **kwargs):
     try:
@@ -16,42 +19,70 @@ def safe_check(function, *args, **kwargs):
     except Exception:
         return "???"
 
+def at_most_x_lines_in_common(project, myrepo_url):
+    try:
+        myrepo_string = open(myrepo_url, "r").read()
 
-scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
-creds = ServiceAccountCredentials.from_json_keyfile_name('credentials.json', scope)
-client = gspread.authorize(creds)
-worksheet = client.open("data 2.4").sheet1
+        maximum = 0
+        for repo_file_url in glob(f"repo_sources/{project}/*"):
+            if(repo_file_url == myrepo_url or exc_command(f"cat {repo_file_url} | wc -l") > TO_MUCH_CODE_THRESHHOLD):
+                continue
+            maximum = max(maximum, lines(myrepo_string, open(repo_file_url, "r").read()))
+            print(".", end="")
 
-projects = ["1-2"] # , "2-1", "2-2", "2-3"]
-count= 140
+        return maximum
 
-data = { 
-    "project": worksheet.range(f'A2:A{count}'), 
-    "url": worksheet.range(f'B2:B{count}'),
-    "commits": worksheet.range(f'C2:C{count}'),
-    "lines of java code": worksheet.range(f'D2:D{count}')
-}
+    except:
+        return "???"
 
-for project in projects:
+def main():
+    scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
+    creds = ServiceAccountCredentials.from_json_keyfile_name('credentials.json', scope)
+    client = gspread.authorize(creds)
+    worksheet = client.open("data 2.4").sheet1
 
-    file = open(project + ".txt")
+    projects = ["1-2"] #, "2-1", "2-2", "2-3"]
+    count= 250
+
+    data = { 
+        "project": worksheet.range(f'A2:A{count}'), 
+        "url": worksheet.range(f'B2:B{count}'),
+        "commits": worksheet.range(f'C2:C{count}'),
+        "lines of code": worksheet.range(f'D2:D{count}'),
+        "file count": worksheet.range(f'E2:E{count}'),
+        "aantal regels exact in andere repo te vinden": worksheet.range(f'F2:F{count}')
+    }
+
     id = 0
+    for project in projects:
 
-    for line in file:
-        if len(line.strip()) < 10:
-            continue
+        file = open(project + ".txt")
 
-        url = line.split(" ")[0].replace("\n", "")
-        print(id, ":", url)
+        for line in file:
+            if len(line.strip()) < 10:
+                continue
 
-        data["project"][id].value = project
-        data["url"][id].value = url
-        data["commits"][id].value = get_to_know_project(project, "git rev-list --all --count")
-        data["lines of java code"][id].value = get_to_know_project(project, "( find ./ -name '*.java' -print0 | xargs -0 cat ) | wc -l") # TODO add more code extensions
-    
-        id += 1
+            url = line.split(" ")[0].replace("\n", "")
+            dir_name = url.split(".com/")[1].replace("/", "_")
+            print("\n", id, ":", url)
+
+            data["project"][id].value = project
+            data["url"][id].value = url
+            data["commits"][id].value = exc_command(f"cd hanzerepos/{project}/{dir_name} && git rev-list --all --count")
+            count = exc_command(f"cd repo_sources/{project}/ && cat {dir_name}.txt | wc -l")
+            data["lines of code"][id].value = count
+            data["file count"][id].value = exc_command(f"find hanzerepos/{project}/{dir_name} -type f | wc -l")
+            if count < TO_MUCH_CODE_THRESHHOLD:
+                data["aantal regels exact in andere repo te vinden"][id].value = at_most_x_lines_in_common(project, f"repo_sources/{project}/{dir_name}.txt")
+            else:
+                data["aantal regels exact in andere repo te vinden"][id].value = "included other data"
+            id += 1
 
 
-for i, row_name in enumerate(data):
-    worksheet.update_cell(1, i+1, row_name)
-    worksheet.update_cells(data[row_name])
+    for i, row_name in enumerate(data):
+        worksheet.update_cell(1, i+1, row_name)
+        worksheet.update_cells(data[row_name])
+
+
+if __name__ == "__main__":
+    main()
